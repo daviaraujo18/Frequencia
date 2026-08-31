6294918bc9bd17e96260988878c46d67b85590c8
wilkersilva101 <wilker.silvapi@bol.com.br>
2026-08-31 16:03:47 -0300

    Paginação & filtros

--- api-ponto/app/controllers/admin/frequentadores_controller.rb
+++ api-ponto/app/controllers/admin/frequentadores_controller.rb
@@ -1,16 +1,32 @@
 module Admin
   class FrequentadoresController < Admin::ApplicationController
     def index
-      @frequentadores = params[:inativos].present? ? User.all : User.ativos
+      if params[:status_filtro].present?
+        @frequentadores = User.where(status: params[:status_filtro])
+      elsif params[:inativos].present?
+        @frequentadores = User.all
+      else
+        @frequentadores = User.ativos
+      end
+
       @frequentadores = @frequentadores.order(:nome_completo)
 
       if params[:nome].present?
         @frequentadores = @frequentadores.where("nome_completo ILIKE ?", "%#{params[:nome]}%")
       end
 
-      if params[:sem_digital].present?
-        @frequentadores = @frequentadores.where(digitais_hash: nil)
+      if params[:digital].present?
+        if params[:digital] == "1"
+          @frequentadores = @frequentadores.where.not(digitais_hash: [nil, ""])
+        elsif params[:digital] == "0"
+          @frequentadores = @frequentadores.where(digitais_hash: [nil, ""])
+        end
+      elsif params[:sem_digital].present?
+        @frequentadores = @frequentadores.where(digitais_hash: [nil, ""])
       end
+
+      @frequentadores = @frequentadores.page(params[:page])
     end
   end
 end
+
